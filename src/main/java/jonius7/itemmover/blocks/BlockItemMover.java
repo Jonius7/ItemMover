3f6df5c9c2f2b2df7c1b8b16532b735aeeafb63e
Jonius7 <jhgenius01@hotmail.com>
2025-10-14 15:07:42 +1100

    Drop Internal Inventory contents on block break

--- src/main/java/jonius7/itemmover/blocks/BlockItemMover.java
+++ src/main/java/jonius7/itemmover/blocks/BlockItemMover.java
@@ -1,8 +1,14 @@
 package jonius7.itemmover.blocks;
 
+import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -32,5 +38,50 @@ public class BlockItemMover extends BlockContainer {
             player.openGui(jonius7.itemmover.ItemMover.instance, 0, world, x, y, z);
         }
         return true;
+    }
+    
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+        TileEntity tile = world.getTileEntity(x, y, z);
+        if (tile instanceof TileEntityItemMover) {
+            dropInventoryItems(world, x, y, z, (TileEntityItemMover) tile);
+            world.func_147453_f(x, y, z, block); // vanilla method: updates comparators
+        }
+
+        super.breakBlock(world, x, y, z, block, meta);
+    }
+    
+    private void dropInventoryItems(World world, int x, int y, int z, TileEntityItemMover te) {
+        Random rand = world.rand;
+
+        for (int i = 0; i < te.getSizeInternalInventory(); i++) {
+            ItemStack stack = te.getInternalStackInSlot(i);
+            if (stack == null) continue;
+
+            float offsetX = rand.nextFloat() * 0.8F + 0.1F;
+            float offsetY = rand.nextFloat() * 0.8F + 0.1F;
+            float offsetZ = rand.nextFloat() * 0.8F + 0.1F;
+
+            EntityItem entityItem = new EntityItem(
+                    world,
+                    x + offsetX,
+                    y + offsetY,
+                    z + offsetZ,
+                    stack.copy()
+            );
+
+            if (stack.hasTagCompound()) {
+                entityItem.getEntityItem().setTagCompound(
+                        (NBTTagCompound) stack.getTagCompound().copy()
+                );
+            }
+
+            float velocity = 0.05F;
+            entityItem.motionX = rand.nextGaussian() * velocity;
+            entityItem.motionY = rand.nextGaussian() * velocity + 0.2F;
+            entityItem.motionZ = rand.nextGaussian() * velocity;
+
+            world.spawnEntityInWorld(entityItem);
+        }
     }
 }
